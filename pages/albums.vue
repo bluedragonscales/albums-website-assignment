163524153d8829bcb07647b5f7f5c67dc3d5ac10
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-08-22 13:31:45 -0700

    Adding drag and drop files

--- pages/albums.vue
+++ pages/albums.vue
@@ -1,12 +1,13 @@
 <template>
     <NavBar></NavBar>
 
-    <div class="container text-center">
+    <div class="container text-center main-create">
         <!-- Bootstrap Grid section. -->
         <h2 class="p-3">Create Your Albums</h2>
 
         <!-- These are the little color boxes at the top to choose a background color. Click on them to activate the method "changeColor"-->
-        <div class="container text-start">
+        <div class="container central-container">
+
             <div class="row">
                 <div class="col">
                     <p>Choose your background color:</p>
@@ -27,18 +28,23 @@
                     <div class="color-box bg-warning" @click="changeColor('#FFC107')"></div>
                 </div>
             </div>
-        </div>
 
 
-        <!-- Main canvas for creating the album. -->
-        <div class="main-canvas text-center m-3" id="canvas" style="height: 100vh; width: auto; border: 5px solid black;"></div>
+            <!-- Main canvas for creating the album. -->
+            <div class="main-canvas text-center m-3" id="canvas" style="height: 100vh; width: auto; border: 5px solid black;">
+                <div class="image-drop">
+                    <DropZone @drop.prevent="dropImage"></DropZone>
+                </div>
+            </div>
 
 
-        <!-- Buttons for adding pages, deleting pages, and publishing your album. -->
-        <div class="text-center p-3">
-            <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to add a new page to your album.">Add Page</button>
-            <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to delete the current page of your album.">Delete Page</button>
-            <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to publish your album so you can start sharing it with friends and family.">Publish Album</button>
+            <!-- Buttons for adding pages, deleting pages, and publishing your album. -->
+            <div class="text-center p-3">
+                <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to add a new page to your album.">Add Page</button>
+                <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to delete the current page of your album.">Delete Page</button>
+                <button type="button" name="button" class="btn btn-outline-info m-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" title="Click here to publish your album so you can start sharing it with friends and family.">Publish Album</button>
+            </div>
+
         </div>
     </div>
 
@@ -47,10 +53,28 @@
 
 
 <script>
+    import { DropZone } from 'dropzone-vue';
+    import { ref } from 'vue';
 
     export default {
         data() {
             return {
+                miscImages: [
+                    'Beach7',
+                    'Hotel2',
+                    'Hotel5',
+                    'pic1',
+                    'pic2',
+                    'pic3',
+                    'pic4',
+                    'pic5',
+                    'pic6',
+                    'pic7',
+                    'pic8',
+                    'pic9',
+                    'pic10',
+                    'pic11'
+                ]
             }
         },
         methods: {
@@ -60,6 +84,19 @@
                 // Changing the background color depending on which color is clicked on in the color divs at the top.
                 mainCanvas.style.background = color;
             }
+        },
+        components: {
+            DropZone
+        },
+        setup() {
+
+            const dropzoneFile = ref("");
+
+            const dropImage = (ev) => {
+                dropzoneFile.value = ev.dataTransfer.files[0]
+            }
+
+            return dropImage;
         }
     }
 
@@ -74,6 +111,17 @@
         width: 30px;
         height: 30px;
         border: 1px solid black;
+    }
+
+    .image-drop {
+        width: 60%;
+        margin: 1rem;
+        height: 60%;
+        border: 2px solid black;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: rgb(243, 243, 243);
     }
 
 </style>
