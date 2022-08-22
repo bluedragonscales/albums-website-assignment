d17f1f263f246523051e04c6c27915ed619eebf1
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-08-22 11:01:25 -0700

    Added first images

--- pages/templates.vue
+++ pages/templates.vue
@@ -8,7 +8,7 @@
 
         <!-- Dynamically rendered images using the "v-for" vue directive. Used props to pass these images from main App file to this child component, including binding the source, alt, and title attributes from the image objects. -->
         <div class="text-center">
-            <img v-for="item in templates" v-bind:key="item" :src="require('../assets' + item.source + '.png')" :alt="item.alt" :title="item.title" class="img-fluid p-3" width="250" height="250">
+            <img v-for="(image, i) in templates" :key="i" :src="require(`~/assets/${image}.png`)" alt="Album template" title="Album template" class="img-fluid p-3" width="250" height="250">
         </div>
     </div>
 
@@ -21,24 +21,15 @@
     export default {
         data() {
             return {
-                
+                templateImages: [
+                    'template-blank',
+                    'template1',
+                    'template2',
+                    'template3',
+                    'template4',
+                    'template5'
+                ]
             }
-        },
-        // Injecting items from the main App file to this child component via props.
-        props: {
-            templates: Array
-        },
-        // Used a mixin to add a border to any image that is clicked on.
-        // mixins: [borderMixin]
+        }
     }
 </script>
-
-
-
-<style scoped>
-
-    .border-for-image {
-        border: 3px solid #67D9F7;
-    }
-
-</style>
