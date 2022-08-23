0f2173547f11ed362adff8d14dc2870e4f1a51c8
KKarnoski <82429567+bluedragonscales@users.noreply.github.com>
2022-08-23 12:41:34 -0700

    Added NuxtLink

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -4,7 +4,7 @@
     <nav class="navbar navbar-expand-lg bg-light">
         <div class="container-fluid">
             <!-- Main logo which can be clicked to go back to home page and reload the website. -->
-            <a class="navbar-brand text-info p-4" href="/">Famous Albums</a>
+            <NuxtLink class="navbar-brand text-info p-4" to="/">Famous Albums</NuxtLink>
             <!-- The nav button that is folded on smaller screens. -->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -14,16 +14,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        <NuxtLink class="nav-link active" to="/">Home</NuxtLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/templates">Album Templates</a>
+                        <NuxtLink class="nav-link" to="/templates">Album Templates</NuxtLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/content">Your Content</a>
+                        <NuxtLink class="nav-link" to="/content">Your Content</NuxtLink>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="/albums">Your Albums</a>
+                        <NuxtLink class="nav-link" to="/albums">Your Albums</NuxtLink>
                     </li>
                 </ul>
             </div>
